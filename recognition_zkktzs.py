1e6835218e93237627211b970c1fc25f92e4d237
JIANRONG Ke <jxk970@cs.bham.ac.uk>
2021-04-16 16:41:50 +0800

    add chinese comment

--- recognition_zkktzs.py
+++ recognition_zkktzs.py
@@ -26,30 +26,37 @@ def read_trurl():
     return data
 def getKeyData(text):
     i = 0
-    key_data = {}
+    key_data = {'partyName':'', 'caseNo':'', 'idNo':'', 'phone':'', 'amount':'', 'acctNo':'', 'acctName':'',
+                'bankName':'', 'bankCode':'', 'department':''}
     department = ''
+    text_del = '部门名称:'
     for one in text:
-        if '当事人姓名' in one[1]:
+        if '当事人姓名' in one[1] and '案件编号' not in text[i+1][1]:
             key_data['partyName'] = text[i+1][1]
-        if '案件编号' in one[1]:
+        if '案件编号' in one[1] and '身份证' not in text[i+1][1]:
             key_data['caseNo'] = text[i+1][1]
-        if '身份证' in one[1] and '代理人' not in one[1]:
+        if '身份证' in one[1] and '代理人' not in one[1] and '联系方式' not in text[i+1][1]:
             key_data['idNo'] = text[i+1][1]
-        if '联系方式' in one[1]:
+        if '联系方式' in one[1] and '金额' not in text[i+1][1]:
             key_data['phone'] = text[i+1][1]
-        if '金额' in one[1]:
+        if '金额' in one[1] and '当事人' not in text[i+1][1]:
             key_data['amount'] = text[i+1][1]
-        if '账号' in one[1]:
+        if '账号' in one[1] and '退款账户' not in text[i+1][1]:
             key_data['acctNo'] = text[i+1][1]
-        if '户名' in one[1]:
+        if '户名' in one[1] and '账户' not in text[i+1][1]:
             key_data['acctName'] = text[i+1][1]
-        if '开户行' in one[1] and '行号' not in one[1]:
+        if '开户行' in one[1] and '行号' not in one[1] and '行号' not in text[i+1][1]:
             key_data['bankName'] = text[i+1][1]
         if '开户行行号' in one[1] and '当事人' not in text[i+1][1]:
             key_data['bankCode'] = text[i+1][1]
         if department == '' and ('派出所' in one[1] or '大队' in one[1] or '看守所' in one[1] or '拘留所' in one[1] or '水利' in one[1]):
-            key_data['department'] = one[1]
-            department = one[1]
+            if text_del in one[1]:
+                department_transfer = one[1].replace(text_del, '', 1)
+                key_data['department'] = department_transfer
+                department = department_transfer
+            else:
+                key_data['department'] = one[1]
+                department = one[1]
         i += 1
     return key_data
 def imgFile_recognition(file):
@@ -79,39 +86,3 @@ if __name__ == '__main__':
     for v, k in key.items():
         print('{v}:{k}'.format(v=v, k=k))
     print(key)
-
-    '''
-    url = 'http://192.168.10.41:8089/api/tr-run/'
-    def img_to_base64(img_path):
-        with open(img_path, 'rb')as read:
-            b64 = base64.b64encode(read.read())
-        return b64
-    img_b64 = img_to_base64('data/test7.jpg')
-    res = requests.post(url=url, data={'img': img_b64})
-    res = res.json()
-    text = res['data']['raw_out']
-    text_simple = []
-    for one in text:
-        one_simple = []
-        one_simple.append(one[1])
-        one_simple.append(one[2])
-        print(one)
-        text_simple.append(one_simple)
-    print(text_simple)
-    
-    img1_file = {
-        'file': open('data/test7.jpg', 'rb')
-    }
-    res = requests.post(url=url, data={'compress': 0}, files=file)
-    res = res.json()
-    text = res['data']['raw_out']
-    print(text)
-    text_simple = []
-    for one in text:
-        one_simple = []
-        one_simple.append(one[1])
-        one_simple.append(one[2])
-        print(one)
-        text_simple.append(one_simple)
-    print(text_simple)
-    '''
--- test.py
+++ test.py
@@ -3,8 +3,8 @@ import requests
 
 if __name__ == '__main__':
     user_info = {'model': 'ZKKTZS'}
-    file = open('data/case1.jpg', 'rb')
-    r = requests.post("http://192.168.10.41:8080/api/v1/AI_detect", data=user_info, files = {'image': file})
+    file = open('data/test-90.jpg', 'rb')
+    r = requests.post("http://127.0.0.1:5000/api/v1/AI_detect", data=user_info, files = {'image': file})
     print(r.json())
 '''
 if __name__ == '__main__':
