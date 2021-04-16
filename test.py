2bc048daecdd5b73afa3b13a7da9835c62a85ce3
JIANRONG Ke <jxk970@cs.bham.ac.uk>
2021-04-16 15:30:17 +0800

    add chinese comment

--- test.py
+++ test.py
@@ -4,7 +4,7 @@ import requests
 if __name__ == '__main__':
     user_info = {'model': 'ZKKTZS'}
     file = open('data/case1.jpg', 'rb')
-    r = requests.post("http://127.0.0.1:5000/api/v1/AI_detect", data=user_info, files = {'image': file})
+    r = requests.post("http://192.168.10.41:8080/api/v1/AI_detect", data=user_info, files = {'image': file})
     print(r.json())
 '''
 if __name__ == '__main__':
