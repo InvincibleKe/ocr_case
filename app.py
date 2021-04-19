39903cf2b01db5942c6b182fa26be33bb8a9ba5c
JIANRONG Ke <jxk970@cs.bham.ac.uk>
2021-04-19 11:29:47 +0800

    modify app.py

--- app.py
+++ app.py
@@ -37,25 +37,38 @@ def upload():
 if __name__ == '__main__':
     app.run(debug=True)
 '''
-from flask import Flask, request
-from flask_restful import Resource, Api, reqparse, abort
+from flask import Flask, request, Response
+from flask_restful import Resource, Api, reqparse, abort, Headers
 from flask_cors import CORS
 import json
 import recognition_zkktzs
+class MyResponse(Response):
+    def __init__(self, response=None, **kwargs):
+        kwargs['headers'] = ''
+        headers = kwargs.get('headers')
+        # 跨域控制
+        origin = ('Access-Control-Allow-Origin', '*')
+        header = ('Access-Control-Allow-Headers', 'Content-Type')
+        methods = ('Access-Control-Allow-Methods', 'HEAD, OPTIONS, GET, POST, DELETE, PUT')
+        if headers:
+            headers.add(*origin)
+            headers.add(*header)
+            headers.add(*methods)
+        else:
+            headers = Headers([origin, header, methods])
+        kwargs['headers'] = headers
+        return super().__init__(response, **kwargs)
 app = Flask(__name__)
+app.response_class(MyResponse)
 api = Api(app)
 #CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 class Product(Resource):
-    def options(self):
-        return {'Allow': '*'}, 200, {'Access-Control-Allow-Origin': '*',
-                                     'Access-Control-Allow-Methods': 'HEAD, OPTIONS, GET, POST, DELETE, PUT',
-                                     'Access-Control-Allow-Headers': 'Content-Type, Content-Length, Authorization, Accept, X-Requested-With , yourHeaderFeild',
-                                     }
     def get(self):
         return {'msg': 'Please use POST method'}
     def post(self):
         parser = reqparse.RequestParser()
         parser.add_argument('model', type=str)
+        parser.add_argument('image64', type=str)
         args = parser.parse_args()
         model = args['model']
         image_file = request.files['image']
