39da22050d8794277677a614164bc172d4dc24fa
JIANRONG Ke <jxk970@cs.bham.ac.uk>
2021-04-19 14:05:01 +0800

    modify app.py

--- start.sh
+++ start.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-exec gunicorn -w 4 -b 0.0.0.0:8080 --worker-class=gevent --worker-connections=200 app:app
+exec gunicorn -w 4 -b 0.0.0.0:5000 --worker-class=gevent --worker-connections=200 app:app
